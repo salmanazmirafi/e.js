7436b6ad799a8730e9f36870ce5b28ff5bcf26cf
Salman Azmi Rafi <salmanazmirafipersonal@gmail.com>
2022-11-24 00:45:33 +0600

    done

--- E-js/index.js
+++ E-js/index.js
@@ -1,14 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = process.env.PROT;
 
+app.use(express.urlencoded({ extended: true }));
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
+let plangusees = [];
+app.get("/", (req, res) => {
+  res.render("index", { pName: plangusees });
+});
+app.post("/", (req, res) => {
+  const programing = req.body.planguse;
+  plangusees.push(programing);
+  res.redirect("/");
+});
 
-app.get('/', (req, res) => {
-  res.render("index", {})
-})
+app.get("/contact", (req, res) => {
+  res.render("contact", {});
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
